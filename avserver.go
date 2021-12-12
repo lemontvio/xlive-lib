0c4ac288e76044f08aef384908c2bb6d076594f2
lemontvio <lemontvio@163.com>
2021-12-12 17:08:18 +0800

    mod avserver

--- avserver.go
+++ avserver.go
@@ -1,8 +1,13 @@
 package avlib
 
 type AVServer interface {
+	Initialize(Config)
 	Serve()
 	Start()
 	Stop()
 	Error() chan error
+	String() string
+	Version() string
 }
+
+type AVServerCall func() AVServer
